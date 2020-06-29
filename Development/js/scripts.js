62bb3783f8ae11fb4062dd3e20194704815dae6d
kawthernasser <66731849+kawthernasser@users.noreply.github.com>
2020-06-29 22:17:28 +0300

    DONE

---
+++ Development/js/scripts.js
@@ -0,0 +1,57 @@
+
+
+let menu=["pasta","ceasar salad","pizza"
+];
+let guess=prompt("if u choose 0=pasta if u choose 1=ceaser salad if u choose 2 ull get pizza what should u type to get pizza?");
+switch (guess)
+{
+    case"0":
+    alert("WHATS WRONG WITH U")
+    break;
+
+    case"1":
+    alert("WHATS WRONG WITH U ")
+    break;
+
+    case"2":
+    alert("YEP")
+    break;
+}
+
+alert("enter numbers in eng")
+let number1=prompt("enter first num")
+let number2=prompt("enter secound num")
+let math=prompt("enter the calc operation")
+
+switch((math)){
+case"multi":
+alert(parseInt(number1)*parseInt(number2));
+break;
+case"div":
+alert(parseInt(number1)/parseInt(number2));
+break;
+case"add":
+alert(parseInt(number1)+parseInt(number2));
+break;
+case"sub":
+alert(parseInt(number1)-parseInt(number2));
+break;}
+
+
+
+let age=prompt("enter ur age ");
+switch (age)
+{
+    case"12":
+    alert("school")
+    break;
+
+    case"5":
+    alert("KG ")
+    break;
+
+    case"22":
+    alert("collage")
+    break;
+}
+
